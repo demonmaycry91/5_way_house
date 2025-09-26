003d345344bec5a2b92423d9f9a9fe3c8d7db914
demonmaycry91 <jinkun91@gmail.com>
2025-09-27 00:19:30 +0800

    Fix: deploy shell file

--- deploy_init.sh
+++ deploy_init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 確保腳本在任何指令失敗時都會終止
+set -e
+
 # 安裝所有相依套件
 pip install -r requirements.txt
 
@@ -12,4 +15,4 @@ flask auth init-roles
 # 建立預設管理員帳號 (只需要執行一次)
 flask auth create-user root password --role Admin
 
-echo "初始化設定完成。"
+echo "部署建置與初始化設定完成。"
